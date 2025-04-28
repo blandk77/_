31fb863525e282c4defe23a7df877f025116d81d
blandk77 <alexags1983@gmail.com>
2025-04-28 15:39:10 +0530

    Update services.py

--- bot/modules/services.py
+++ bot/modules/services.py
@@ -24,6 +24,15 @@ from bot.helper.telegram_helper.message_utils import (
 
 @new_task
 async def start(client, message):
+        help_command = f"/{BotCommands.HelpCommand}"
+        start_string = "Hello there boss!"
+    unauth_start = "who are you??"
+        sbutton = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("Updates", url="https://t.me/The_TGguy"),
+                 InlineKeyboardButton("Owner", url="t.me/Itsme123i")]  
+            ]
+        )    
     if len(message.command) > 1 and message.command[1] == "private":
         await delete_message(message)
     elif len(message.command) > 1 and message.command[1] == "gensession":
@@ -86,17 +95,10 @@ async def start(client, message):
         msg += f"It will be valid for {get_readable_time(int(Config.TOKEN_TIMEOUT), True)}"
         return await send_message(message, msg)
     elif await CustomFilters.authorized(client, message):
-        help_command = f"/{BotCommands.HelpCommand}"
-        start_string = f"Hello there boss!"
-        sbutton = InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("Updates", url="https://t.me/The_TGguy"),
-                 InlineKeyboardButton("Owner", url="t.me/Itsme123i")]  
-            ]
-        )
-        await send_message(message, start_string, reply_markup=sbutton)
+
+        await send_message(message, start_string, buttons=sbutton)
     else:
-        await send_message(message, "Who are you??", reply_markup=sbutton)
+        await send_message(message, unauth_start, buttons=sbutton)
 
     # Safely update PM users database
     if message.from_user and hasattr(message.from_user, "id"):
